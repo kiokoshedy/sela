092a242f5bef5d03689860d43e2b6ebd4da26d57
kiokoshedy <shkmusembi@gmail.com>
2023-09-27 11:56:25 +0300

    navigation dependencies

--- api/index.js
+++ api/index.js
@@ -1,17 +1,34 @@
-const express = require ("express");
-const bodyParser = require ("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
-const nodemailer = require ("nodemailer");
+const nodemailer = require("nodemailer");
 
 const app = express();
-const port = 800;
+const port = 8000;
 const cors = require("cors");
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-mongoose.connect("mongodb+srv://shkmusembi:kiokoshedy@cluster0.bcszaqh.mongodb.net/")
+mongoose
+  .connect(
+    "mongodb+srv://shkmusembi:kiokoshedy@cluster0.bcszaqh.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB: ", err);
+  });
+
+app.listen(port, () => {
+  console.log("Connected to port 8000.");
+});
