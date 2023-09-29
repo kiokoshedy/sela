6ada2167653adca4cc5be833cdd16703c46006ff
kiokoshedy <shkmusembi@gmail.com>
2023-09-29 14:28:55 +0300

    changes in register screen

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,9 +8,9 @@ import {
   Image,
   KeyboardAvoidingView,
   TextInput,
+  ScrollView,
   Alert,
 } from "react-native";
-import React, { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
@@ -21,6 +22,7 @@ const RegisterScreen = () => {
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const navigation = useNavigation();
+
   const handleRegister = () => {
     const user = {
       name: name,
@@ -28,7 +30,7 @@ const RegisterScreen = () => {
       password: password,
     };
 
-    // send a POST  request to the backend API to register the user
+    // Send a POST request to the backend API to register the user
     axios
       .post("http://localhost:8000/register", user)
       .then((response) => {
@@ -46,186 +48,157 @@ const RegisterScreen = () => {
           "Registration Error",
           "An error occurred while registering"
         );
-        console.log("registration failed", error);
+        console.log("Registration failed", error);
       });
   };
-  return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: "white", alignItems: "center",marginTop:50  }}
-    >
-      <View>
-        <Image
-          style={{ width: 150, height: 100 }}
-          source={{
-            uri: "https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png",
-          }}
-        />
-      </View>
 
-      <KeyboardAvoidingView>
-        <View style={{ alignItems: "center" }}>
-          <Text
-            style={{
-              fontSize: 17,
-              fontWeight: "bold",
-              marginTop: 12,
-              color: "#041E42",
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={{
+              uri: "https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png",
             }}
-          >
-            Register to your Account
-          </Text>
+          />
         </View>
 
-        <View style={{ marginTop: 70 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 5,
-              backgroundColor: "#D0D0D0",
-              paddingVertical: 5,
-              borderRadius: 5,
-              marginTop: 30,
-            }}
-          >
-            <Ionicons
-              name="ios-person"
-              size={24}
-              color="gray"
-              style={{ marginLeft: 8 }}
-            />
+        <KeyboardAvoidingView
+          behavior="padding"
+          style={styles.formContainer}
+          enabled
+        >
+          <Text style={styles.title}>Register to your Account</Text>
+
+          <View style={styles.inputContainer}>
+            <Ionicons name="ios-person" size={24} color="gray" />
             <TextInput
               value={name}
               onChangeText={(text) => setName(text)}
-              style={{
-                color: "gray",
-                marginVertical: 10,
-                width: 300,
-                fontSize: name ? 16 : 16,
-              }}
-              placeholder="enter your name"
+              style={styles.input}
+              placeholder="Enter your name"
             />
           </View>
 
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 5,
-              backgroundColor: "#D0D0D0",
-              paddingVertical: 5,
-              borderRadius: 5,
-              marginTop: 30,
-            }}
-          >
-            <MaterialIcons
-              style={{ marginLeft: 8 }}
-              name="email"
-              size={24}
-              color="gray"
-            />
-
+          <View style={styles.inputContainer}>
+            <MaterialIcons name="email" size={24} color="gray" />
             <TextInput
               value={email}
               onChangeText={(text) => setEmail(text)}
-              style={{
-                color: "gray",
-                marginVertical: 10,
-                width: 300,
-                fontSize: password ? 16 : 16,
-              }}
-              placeholder="enter your Email"
+              style={styles.input}
+              placeholder="Enter your Email"
             />
           </View>
-        </View>
 
-        <View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 5,
-              backgroundColor: "#D0D0D0",
-              paddingVertical: 5,
-              borderRadius: 5,
-              marginTop: 30,
-            }}
-          >
-            <AntDesign
-              name="lock1"
-              size={24}
-              color="gray"
-              style={{ marginLeft: 8 }}
-            />
-
+          <View style={styles.inputContainer}>
+            <AntDesign name="lock1" size={24} color="gray" />
             <TextInput
               value={password}
               onChangeText={(text) => setPassword(text)}
               secureTextEntry={true}
-              style={{
-                color: "gray",
-                marginVertical: 10,
-                width: 300,
-                fontSize: email ? 16 : 16,
-              }}
-              placeholder="enter your Password"
+              style={styles.input}
+              placeholder="Enter your Password"
             />
           </View>
-        </View>
 
-        <View
-          style={{
-            marginTop: 12,
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Text>Keep me logged in</Text>
+          <View style={styles.checkboxContainer}>
+            <Text>Keep me logged in</Text>
+            <Text style={styles.forgotPassword}>Forgot Password</Text>
+          </View>
 
-          <Text style={{ color: "#007FFF", fontWeight: "500" }}>
-            Forgot Password
-          </Text>
-        </View>
-
-        <View style={{ marginTop: 80 }} />
-
-        <Pressable
-          onPress={handleRegister}
-          style={{
-            width: 200,
-            backgroundColor: "#FEBE10",
-            borderRadius: 6,
-            marginLeft: "auto",
-            marginRight: "auto",
-            padding: 15,
-          }}
-        >
-          <Text
-            style={{
-              textAlign: "center",
-              color: "white",
-              fontSize: 16,
-              fontWeight: "bold",
-            }}
+          <Pressable
+            onPress={handleRegister}
+            style={styles.registerButton}
           >
-            Register
-          </Text>
-        </Pressable>
+            <Text style={styles.registerButtonText}>Register</Text>
+          </Pressable>
 
-        <Pressable
-          onPress={() => navigation.goBack()}
-          style={{ marginTop: 15 }}
-        >
-          <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
-            Already have an account? Sign In
-          </Text>
-        </Pressable>
-      </KeyboardAvoidingView>
+          <Pressable
+            onPress={() => navigation.goBack()}
+            style={styles.signInLink}
+          >
+            <Text style={styles.signInText}>
+              Already have an account? Sign In
+            </Text>
+          </Pressable>
+        </KeyboardAvoidingView>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default RegisterScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  logoContainer: {
+    alignItems: "center",
+  },
+  logo: {
+    width: 150,
+    height: 100,
+  },
+  formContainer: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 17,
+    fontWeight: "bold",
+    marginTop: 12,
+    color: "#041E42",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#D0D0D0",
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginTop: 30,
+  },
+  input: {
+    color: "gray",
+    marginVertical: 10,
+    width: 300,
+    fontSize: 16,
+  },
+  checkboxContainer: {
+    marginTop: 12,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  forgotPassword: {
+    color: "#007FFF",
+    fontWeight: "500",
+  },
+  registerButton: {
+    width: 200,
+    backgroundColor: "#FEBE10",
+    borderRadius: 6,
+    marginVertical: 15,
+    padding: 15,
+  },
+  registerButtonText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  signInLink: {
+    marginTop: 15,
+  },
+  signInText: {
+    textAlign: "center",
+    color: "gray",
+    fontSize: 16,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default RegisterScreen;
