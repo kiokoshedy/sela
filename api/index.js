e17127c9c621c7b931c47a29e438d462c7717235
kiokoshedy <shkmusembi@gmail.com>
2023-10-02 23:10:25 +0300

    verify user email

--- api/index.js
+++ api/index.js
@@ -33,5 +33,76 @@ app.listen(port, () => {
   console.log("Connected to port 8000.");
 });
 
+const User = require("./models/user");
+const Order = require("./models/order");
+
+
+//functionn to send verification email
+const sendVerificationEmail = async (email, verificationToken) => {
+  // Create a Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+
+    // Configure the email service or SMTP details here
+    service: "gmail",
+    auth: {
+      user: "skmusembi@gmail.com",
+      pass: "wkkjjprzkqxtboju",
+    },
+  });
+
+  // Compose the email message
+  const mailOptions = {
+    from: "amazon.com",
+    to: email,
+    subject: "Email Verification",
+    text: `Please click the following link to verify your email: http://localhost:8000/verify/${verificationToken}`,
+  };
+
+  // Send the email
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Verification email sent successfully");
+  } catch (error) {
+    console.error("Error sending verification email:", error);
+  }
+};
+// Register a new user
+// ... existing imports and setup ...
+
 //Endpoint to register the app
-app.post
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if the email is already registered
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log("Email already registered:", email); // Debugging statement
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    // Create a new user
+    const newUser = new User({ name, email, password });
+
+    // Generate and store the verification token
+    newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+
+    // Save the user to the database
+    await newUser.save();
+
+    // Debugging statement to verify data
+    console.log("New User Registered:", newUser);
+
+    // Send verification email to the user
+    // Use your preferred email service or library to send the email
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+    res.status(201).json({
+      message:
+        "Registration successful. Please check your email for verification.",
+    });
+  } catch (error) {
+    console.log("Error during registration:", error); // Debugging statement
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
